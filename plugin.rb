896295d79790e2c42b7e406385685bc20ba048b9
Vinoth Kannan <vinothkannan@vinkas.com>
2016-11-15 23:37:25 +0530

    Initial commit of Discourse Wrapper

---
+++ plugin.rb
@@ -0,0 +1,5 @@
+# name: discourse-wrapper
+# about: Customize your Discourse header, footer, banner and navigation sections
+# version: 0.0.1
+# authors: Vinoth Kannan (vinothkannan@vinkas.com)
+# url: https://github.com/vinkas0/discourse-wrapper
